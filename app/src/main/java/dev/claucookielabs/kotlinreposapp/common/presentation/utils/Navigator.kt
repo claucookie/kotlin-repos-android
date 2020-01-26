be09fec7877d53645e750a53c72c0e64b500c89e
claucookie <claudia.luque.fernandez@gmail.com>
2020-01-26 18:26:38 +0000

    Add click listener and navigator

---
+++ app/src/main/java/dev/claucookielabs/kotlinreposapp/common/presentation/utils/Navigator.kt
@@ -0,0 +1,11 @@
+package dev.claucookielabs.kotlinreposapp.common.presentation.utils
+
+import android.content.Context
+import dev.claucookielabs.kotlinreposapp.common.domain.model.Repo
+
+class Navigator {
+
+    fun navigateToDetailScreen(context: Context, repo: Repo) {
+        // Create activity, Start intent and pass repo data
+    }
+}

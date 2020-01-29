bfb21d72480e66df9856c5529299fd970f01afb9
claucookie <claudia.luque.fernandez@gmail.com>
2020-01-29 08:57:37 +0000

    Fix dependency not found for GetListOfRepos

--- app/src/main/java/dev/claucookielabs/kotlinreposapp/reposlist/domain/GetListOfRepos.kt
+++ app/src/main/java/dev/claucookielabs/kotlinreposapp/reposlist/domain/GetListOfRepos.kt
@@ -1,12 +1,12 @@
 package dev.claucookielabs.kotlinreposapp.reposlist.domain
 
-import dev.claucookielabs.kotlinreposapp.common.data.repository.GithubDataRepository
 import dev.claucookielabs.kotlinreposapp.common.domain.BaseRequest
+import dev.claucookielabs.kotlinreposapp.common.domain.GithubRepository
 import dev.claucookielabs.kotlinreposapp.common.domain.UseCase
 import dev.claucookielabs.kotlinreposapp.common.domain.model.Repo
 import dev.claucookielabs.kotlinreposapp.common.domain.model.ResultWrapper
 
-class GetListOfRepos(private val githubRepository: GithubDataRepository) :
+class GetListOfRepos(private val githubRepository: GithubRepository) :
     UseCase<GetReposRequest, ResultWrapper<List<Repo>>> {
 
     override suspend fun execute(request: GetReposRequest): ResultWrapper<List<Repo>> {
